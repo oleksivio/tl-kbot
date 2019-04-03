ae69c19d80e8fcb3162925ba8536c728911606e1
Ilya <oleksivio@gmail.com>
2019-04-04 01:00:59 +0300

    Fix sendPhoto serialization

--- src/main/kotlin/io/github/oleksivio/telegram/bot/api/model/method/file/SendPhoto.kt
+++ src/main/kotlin/io/github/oleksivio/telegram/bot/api/model/method/file/SendPhoto.kt
@@ -19,27 +19,32 @@ class SendPhoto(
          * exists on the Telegram servers (recommended), pass an HTTP URL as a String for Telegram to
          * get a photo from the Internet
          */
-        @JsonProperty("photo")
+        @JsonProperty(ApiDict.PHOTO_KEY)
         val photo: String,
         /**
          * caption	String	Optional	Document caption (may also be used when resending documents by file_id), 0-1024 characters
          */
+        @JsonProperty(ApiDict.CAPTION_KEY)
         override val caption: String? = null,
         /**
          * parse_mode	String	Optional	Send Markdown or HTML, if you want Telegram apps to show bold, italic, fixed-width text or inline URLs in the media caption.
          */
+        @JsonProperty(ApiDict.PARSE_MODE_KEY)
         override val parseMode: String? = null,
         /**
          * disable_notification	Boolean	Optional	Sends the message silently. Users will receive a notification with no sound.
          */
+        @JsonProperty(ApiDict.DISABLE_NOTIFICATION_KEY)
         override val disableNotification: Boolean? = null,
         /**
          * reply_to_message_id	Integer	Optional	If the message is a reply, ID of the original message
          */
+        @JsonProperty(ApiDict.REPLY_TO_MESSAGE_ID_KEY)
         override val replyToMessageId: Long? = null,
         /**
          * reply_markup	InlineKeyboardMarkup or ReplyKeyboardMarkup or ReplyKeyboardRemove or ForceReply	Optional	Additional interface options. A JSON-serialized object for an inline keyboard, custom reply keyboard, instructions to remove reply keyboard or to force a reply from the user.
          */
+        @JsonProperty(ApiDict.REPLY_MARKUP_KEY)
         override val replyMarkup: IKeyboard? = null
 ) : CaptionAction() {
 
