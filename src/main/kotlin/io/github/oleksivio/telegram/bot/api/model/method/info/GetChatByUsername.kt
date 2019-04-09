caf9d56829aa7d99ba85a508b6647964b2153ff6
Ilya <ilia.oleksiv@gmail.com>
2019-04-09 22:48:53 +0300

    Add chatByUsername

--- src/main/kotlin/io/github/oleksivio/telegram/bot/api/model/method/info/GetChatByUsername.kt
+++ src/main/kotlin/io/github/oleksivio/telegram/bot/api/model/method/info/GetChatByUsername.kt
@@ -1,22 +1,22 @@
-package io.github.oleksivio.telegram.bot.api.model.method.group
+package io.github.oleksivio.telegram.bot.api.model.method.info
 
 import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonProperty
 import io.github.oleksivio.telegram.bot.api.model.objects.std.Chat
 import io.github.oleksivio.telegram.bot.core.model.ApiDict
 import io.github.oleksivio.telegram.bot.core.model.ChatResponse
-import io.github.oleksivio.telegram.bot.core.model.method.common.ChatAction
+import io.github.oleksivio.telegram.bot.core.model.method.Action
 
 /**
  * @see [getChat](https://core.telegram.org/bots/api/#getchat)
  */
-data class GetChat(
+data class GetChatByUsername(
         /**
-         * chat_id Integer or String Yes Unique identifier for the target chat or username of the target channel
+         * chat_id  String Yes username of the target channel
          */
         @JsonProperty(ApiDict.CHAT_ID_KEY)
-        override val chatId: Long
-) : ChatAction<Chat>() {
+        val chatId: String
+) : Action<Chat>() {
 
     @JsonIgnore
     override val resultWrapperClass = ChatResponse::class
