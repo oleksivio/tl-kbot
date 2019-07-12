3aca177bca346fbf04c6ba5cfb8424636b7f1c5c
Ilia Oleksiv <ilia.oleksiv@gmail.com>
2019-07-12 19:17:26 +0300

    feat(gradle): Add nebula plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,9 @@ plugins {
     val kotlinVersion = "1.3.41"
     kotlin("jvm") version (kotlinVersion)
     id("org.jlleitschuh.gradle.ktlint") version "8.1.0"
+    id("nebula.release") version "10.1.2"
 }
 
 val archivesBaseName = "tl-kbot"
 
-group = "io.github.oleksivio"
+group = "io.github.oleksivio.tl.kbot"
--- spring/build.gradle.kts
+++ spring/build.gradle.kts
@@ -4,8 +4,15 @@ plugins {
     kotlin("jvm")
     val kotlinVersion = "1.3.41"
     id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
+    id("nebula.maven-publish") version "10.0.1"
+    id("nebula.javadoc-jar") version "10.0.1"
+    id("nebula.source-jar") version "10.0.1"
+    signing
 }
 
+val uploadUsernameProp: String? by project
+val uploadPasswordProp: String? by project
+val releaseBuild: String? by project
 
 repositories {
     mavenCentral()
@@ -31,3 +38,56 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "11"
 }
 
+
+signing {
+    isRequired = releaseBuild?.toBoolean() ?: false
+    sign(publishing.publications["nebula"])
+}
+
+publishing {
+    publications {
+        getByName<MavenPublication>("nebula") {
+            pom {
+                setDescription("Kotlin spring telegram bot api library")
+
+                name.set("Kotlin Telegram Bot Api")
+                url.set("https://github.com/oleksivio/telegram-bot-api")
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/oleksivio/telegram-bot-api.git")
+                    developerConnection.set("scm:git:ssh://github.com/oleksivio/telegram-bot-api.git")
+                    url.set("http://github.com/oleksivio/telegram-bot-api/")
+                }
+                developers {
+                    developer {
+                        id.set("oleksivio")
+                        name.set("Ilia Oleksiv")
+                        email.set("ilia.oleksiv@gmail.com")
+                    }
+                }
+
+            }
+        }
+    }
+    repositories {
+        maven {
+            credentials {
+                username = uploadUsernameProp
+                password = uploadPasswordProp
+
+                username ?: println("Upload USERNAME not set")
+                password ?: println("Upload PASSWORD not set")
+            }
+
+            val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2")
+            val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+        }
+    }
+}
