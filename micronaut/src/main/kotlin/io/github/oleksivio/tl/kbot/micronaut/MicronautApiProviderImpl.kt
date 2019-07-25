6e844f4f1ffe856115616ac294d8c50eef97bfe8
Ilia Oleksiv <ilia.oleksiv@gmail.com>
2019-07-25 18:45:32 +0300

    feat(micronaut): Add serverErrorListener to micronaut realisation

--- micronaut/src/main/kotlin/io/github/oleksivio/tl/kbot/micronaut/MicronautApiProviderImpl.kt
+++ micronaut/src/main/kotlin/io/github/oleksivio/tl/kbot/micronaut/MicronautApiProviderImpl.kt
@@ -1,14 +1,18 @@
 package io.github.oleksivio.tl.kbot.micronaut
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import io.github.oleksivio.tl.kbot.core.controller.network.ApiProvider
 import io.github.oleksivio.tl.kbot.core.controller.network.ServerErrorListener
+import io.github.oleksivio.tl.kbot.core.model.ErrorResponse
 import io.github.oleksivio.tl.kbot.server.api.model.method.Action
 import io.micronaut.context.annotation.Value
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.client.DefaultHttpClient
+import io.micronaut.http.client.FullNettyClientHttpResponse
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.http.client.exceptions.HttpClientResponseException
 import org.slf4j.LoggerFactory
+import java.nio.charset.StandardCharsets
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -24,19 +28,25 @@ open class MicronautApiProviderImpl(
         return try {
             val req = HttpRequest.POST("", action)
             val flowable = httpClient.retrieve(req, action.resultWrapperClass.java)
-            val result = flowable.firstElement()
-            result.blockingGet().get()
-        } catch (httpClientException: HttpClientResponseException) {
-            // TODO [serverErrorListener] call
-            LOG.error(
-                "Telegram server error. Check sent params of request and check that user not block bot",
-                httpClientException
-            )
-            null
+
+            val result = flowable.firstElement().doOnError { e ->
+                if (e is HttpClientResponseException) {
+                    val errorResponse = parseResponseString(e)
+                    serverErrorListener(errorResponse)
+                }
+            }.blockingGet()
+            
+            result.get()
         } catch (e: Exception) {
             LOG.error("", e)
             null
         }
+    }
+
+    private fun parseResponseString(e: HttpClientResponseException): ErrorResponse {
+        val fullNettyClientHttpResponse = e.response as FullNettyClientHttpResponse
+        val respString = fullNettyClientHttpResponse.nativeResponse.content().toString(StandardCharsets.UTF_8)
+        return ObjectMapper().readValue(respString, ErrorResponse::class.java)
     }
 }
 
